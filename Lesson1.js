31ce0ff725a64d0095222f8bf3de285efa9a73ee
VolodymyrHalchyk <ckatbiha1991@gmail.com>
2020-10-27 23:26:22 +0200

    JS lesson 1

--- Lesson1.js
+++ Lesson1.js
@@ -1,6 +1,6 @@
 // - присвоить каждому из следующих значений свою переменную: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14,
 // 2.7, 16, true, false Вывести каждую при помощи console.log, alert, document.write
-
+   
 // let q = 'hello';
 // let w = 'owu';
 // let e = 'com';
